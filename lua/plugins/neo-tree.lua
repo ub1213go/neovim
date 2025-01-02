474ba0fa29537126fbf37b43d07dab65b29820d1
ub1213go <ub1213go@gmail.com>
2025-01-02 22:30:41 +0800

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,6 +8,31 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
+    require('neo-tree').setup({
+    window = {
+        mappings = {
+          ["P"] = function(state)
+            local node = state.tree:get_node()
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end,
+          ["J"] = function(state)
+            local tree = state.tree
+            local node = tree:get_node()
+            local siblings = tree:get_nodes(node:get_parent_id())
+            local renderer = require('neo-tree.ui.renderer')
+            renderer.focus_node(state, siblings[#siblings]:get_id())
+          end,
+          ["K"] = function(state)
+            local tree = state.tree
+            local node = tree:get_node()
+            local siblings = tree:get_nodes(node:get_parent_id())
+            local renderer = require('neo-tree.ui.renderer')
+            renderer.focus_node(state, siblings[1]:get_id())
+          end
+        }
+      }
+    })
+
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
   end
 }
