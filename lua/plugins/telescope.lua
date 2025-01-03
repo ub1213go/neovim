cee2824604b1231a75a34ac05d12ec52961e45ad
明倫 蔡 <alan-tsai@sag.com.tw>
2025-01-03 08:36:34 +0800

    chore: 修改gd to <S-F12>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
-      vim.keymap.set('n', 'gd', builtin.lsp_type_definitions, {})
+      vim.keymap.set('n', '<S-F12>', builtin.lsp_type_definitions, {})
       vim.keymap.set('n', '<C-F12>', builtin.lsp_implementations, {})
     end
   },
