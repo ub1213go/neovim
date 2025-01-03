8aa7f74e712885dfe61ee1f8df1e2df11fafc1f4
明倫 蔡 <alan-tsai@sag.com.tw>
2025-01-03 16:51:27 +0800

    chore: update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,8 +8,8 @@ return {
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
-      -- vim.keymap.set('n', '<S-F12>', builtin.lsp_type_definitions, {})
-      -- vim.keymap.set('n', '<C-F12>', builtin.lsp_implementations, {})
+      vim.keymap.set('n', '<S-F12>', builtin.lsp_type_definitions, {})
+      vim.keymap.set('n', '<C-F12>', builtin.lsp_implementations, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
