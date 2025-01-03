093725de1e34cb7e21c5f4efef5129eff0e905c4
ub1213go <ub1213go@gmail.com>
2025-01-04 07:51:41 +0800

    update

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -28,11 +28,36 @@ return {
             local siblings = tree:get_nodes(node:get_parent_id())
             local renderer = require('neo-tree.ui.renderer')
             renderer.focus_node(state, siblings[1]:get_id())
-          end
+          end,
+          -- disable fuzzy finder 
+          ["/"] = "noop"
         }
       }
+
     })
 
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>n', function()
+          local reveal_file = vim.fn.expand('%:p')
+          if (reveal_file == '') then
+            reveal_file = vim.fn.getcwd()
+          else
+            local f = io.open(reveal_file, "r")
+            if (f) then
+              f.close(f)
+            else
+              reveal_file = vim.fn.getcwd()
+            end
+          end
+          require('neo-tree.command').execute({
+            action = "focus",          -- OPTIONAL, this is the default value
+            source = "filesystem",     -- OPTIONAL, this is the default value
+            position = "left",         -- OPTIONAL, this is the default value
+            reveal_file = reveal_file, -- path to file or folder to reveal
+            reveal_force_cwd = true,   -- change cwd without asking if needed
+          })
+        end,
+        { desc = "Open neo-tree at current file or working directory" }
+      );
   end
 }
