c898a8294b118dbb399c52824e3a31073466f313
ub1213go <ub1213go@gmail.com>
2025-01-03 19:41:29 +0800

    udpate

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,11 +7,12 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
-      vim.keymap.set('n', '<S-F12>', builtin.lsp_type_definitions, {})
-      vim.keymap.set('n', '<C-F12>', builtin.lsp_implementations, {})
+      -- vim.keymap.set('n', '<leader>gd', builtin.lsp_definitions, {})
+      -- vim.keymap.set('n', '<S-F12>', builtin.lsp_type_definitions, {})
+      -- vim.keymap.set('n', '<C-F12>', builtin.lsp_implementations, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
     end
   },
   {
