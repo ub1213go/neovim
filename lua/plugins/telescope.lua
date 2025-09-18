75b66874d642389089934bd2c4d7f621260f3a27
明倫 蔡 <alan-tsai@sag.com.tw>
2025-09-18 17:08:46 +0800

    u

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -125,19 +125,8 @@ return {
       {"<leader>sp", function() require("spectre").open_file_search({select_word=true}) end, desc = "在當前檔案搜尋"}
     },
     config = function()
-      require('spectre').setup({
-        color_devicons = true,
-        open_cmd = 'vnew',
-        live_update = false,  -- 避免卡頓
-        line_sep_start = '┌─────────────────────────────────────────',
-        result_padding = '¦  ',
-        line_sep       = '└─────────────────────────────────────────',
-        highlight = {
-          ui = "String",
-          search = "DiffChange",
-          replace = "DiffDelete"
-        }
-      })
+      -- 使用完全預設的 Spectre 配置
+      require('spectre').setup()
     end
   },
   
