430c7df13415faf2a4e40a1509ddfb87448a7014
ub1213go <ub1213go@gmail.com>
2025-10-12 19:22:06 +0800

    u

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,52 @@
-
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
+    -- Windows 特定：設定編譯器優先順序
+    require('nvim-treesitter.install').compilers = { "zig", "clang", "gcc" } 
+
     local config = require("nvim-treesitter.configs")
     config.setup({
-      -- ensure_installed = {"lua", "javascript"},
+      -- 明確指定要安裝的語言（避免安裝不必要的 parser）
+      ensure_installed = { 
+        "lua", 
+        "vim", 
+        "vimdoc",
+        "javascript",
+        "typescript",
+        "rust",
+        "markdown",
+        "toml",
+        "vue",
+      },
+      
+      -- 啟用自動安裝（當打開對應文件類型時）
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+      
+      -- 語法高亮
+      highlight = { 
+        enable = true,
+        -- 如果遇到問題可以針對特定語言禁用
+        -- disable = { "rust" },  -- 例如：禁用 rust 的高亮
+      },
+      
+      -- 自動縮排
+      indent = { 
+        enable = true,
+        -- 某些語言的縮排可能有問題，可以選擇性禁用
+        -- disable = { "python" },
+      },
+      
+      -- 增量選擇（可選）
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          scope_incremental = "<S-CR>",
+          node_decremental = "<BS>",
+        },
+      },
     })
   end
 }
